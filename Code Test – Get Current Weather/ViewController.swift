405ba1dc2ae1c35799bc603b4d925a2fc74533ee
Santosh <santosh.guruju@icloud.com>
2019-06-05 19:44:45 +0800

    Initial Commit

---
+++ Code Test – Get Current Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Code Test – Get Current Weather
+//
+//  Created by apple on 05/06/2019.
+//  Copyright © 2019 workstreak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
